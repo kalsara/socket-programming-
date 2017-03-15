40c32127665c27a3d6f8bfa1257651991826aea1
kalsara <kalsaramagamage@gmail.com>
2017-03-15 18:45:38 +0530

    set server variables

---
+++ src/Skeleton2/SimpleServer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Skeleton2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Gab
+ */
+public class SimpleServer {
+    /* 
+     This is to print the date and time 
+     */
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+    /*
+     These variables are to print the details of client and server
+     */
+    InetAddress ipAddress   =   null;
+    String      hostname    =   null;
+    String      message     =   null;
+    /**
+     *
+     * @throws UnknownHostException
+     */
+    
+}
